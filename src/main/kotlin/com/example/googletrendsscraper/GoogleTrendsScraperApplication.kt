a8308832a215503df4b1e2afd3acc3bbc15e7c6d
Franco904 <francostavares2003@gmail.com>
2024-03-16 17:40:22 -0300

    build: spring initializer files

---
+++ src/main/kotlin/com/example/googletrendsscraper/GoogleTrendsScraperApplication.kt
@@ -0,0 +1,11 @@
+package com.example.googletrendsscraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleTrendsScraperApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleTrendsScraperApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/googletrendsscraper/GoogleTrendsScraperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.googletrendsscraper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleTrendsScraperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
